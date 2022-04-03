9433e7501811d4a9b4eb05115c67a53e41b685e4
Dhoni Hanif Supriyadi <66652598+dhonihanif@users.noreply.github.com>
2022-04-04 03:34:35 +0700

    Add files via upload

---
+++ flaskk.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from model import Model
+
+df = pd.read_csv('C:/Users/user/Downloads/archive/SaYoPillow.csv')
+df.columns = ['laju dengkuran', 'laju pernapasan',
+           'suhu tubuh', 'gerakan anggota badan',
+           'oksigen darah', 'gerakan mata',
+           'jam tidur', 'detak jantung','stress level']
+
+app = Flask(__name__)
+model = Model(df)
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+def predict(listt):
+    pred = np.array(listt).reshape(1, -1)
+    result = model.log.predict(pred)
+    return result[0]
+
+@app.route('/', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        laju_dengkuran, laju_pernapasan, suhu_tubuh, gerakan_anggota_badan, oksigen_darah, gerakan_mata,jam_tidur, detak_jantung = request.form['laju dengkuran'], request.form['laju pernapasan'], request.form['suhu tubuh'], request.form['gerakan anggota badan'], request.form['oksigen darah'], request.form['gerakan mata'], request.form['jam tidur'], request.form['detak jantung']
+        result = predict([laju_dengkuran, laju_pernapasan, suhu_tubuh,
+        gerakan_anggota_badan, oksigen_darah, gerakan_mata, jam_tidur, detak_jantung])
+        return render_template('halaman1.html', result=result)
+    else:
+        lanjut = ''
+        return render_template('home.html', result=lanjut )
+if __name__ == '__main__':
+    app.run(debug=True)
+    
